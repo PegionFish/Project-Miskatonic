b4751baa7ca6e953cce2e49baaa8c26ad4557f17
割鱼蜓猪 <42823993+BobGuoYilong@users.noreply.github.com>
2019-05-07 20:21:37 +0800

    ....

--- module/dice.py
+++ module/dice.py
@@ -3,4 +3,9 @@ import re
 
 
 def roll (roll, data)
- 
+    """
+    This function allows you to roll dices when you need to.
+    You should input data like 1d6 for 1 dice up to 6.
+    """
+    
+    
