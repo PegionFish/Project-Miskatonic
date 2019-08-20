ecb4704c028180c837f0f32a4f06303a4d5de1c5
BobGuoYilong <boblao0714@gmail.com>
2019-08-20 15:25:06 +0800

    Tinker GUI

---
+++ main.py
@@ -0,0 +1,6 @@
+import tkinter
+
+top = tkinter.Tk(className = 'Operation Miskatonic')
+top.geometry("1024x768")
+
+top.mainloop()
--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+
+
 with open('player\player_DEMO') as f:
     for line in f.readlines():
         print (line)
+        pass
