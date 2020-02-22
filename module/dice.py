2905d9e6918c24c4632576df9068f737ca472262
鸽鱼 <boblao0714@gmail.com>
2020-02-22 17:08:30 +0800

    remove bloat

--- module/dice.py
+++ module/dice.py
@@ -2,11 +2,7 @@ import random
 import re
 
 def roll (roll, data):
-    """
-    This function allows you to roll dices when you need to.
-    "roll" means how many dices you want to throw at once.
-    "data" means how many sizes do a single dice have.
-    """
+
     class dice
         num = random.randint(1,100)
 print (roll)
